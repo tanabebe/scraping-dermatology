cf712ab8d0c3d4e5e0bddf95fd1a7bc504834f99
tanabebe <n.tanabe@zarathustra.tech>
2019-12-17 12:21:30 +0900

    add headless mode. add delete img.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/sclevine/agouti"
@@ -18,7 +20,7 @@ type ScrapingList struct {
 var Scraping ScrapingList
 var target agouti.WebDriver
 
-// iniファイルの読み込み
+
 func init() {
 	config, err := ini.Load("config.ini")
 
@@ -31,11 +33,37 @@ func init() {
 		Email:    config.Section("login").Key("email").MustString(""),
 		Password: config.Section("login").Key("password").MustString(""),
 	}
+
+	if _, err := os.Stat("waiting"); err != nil {
+		log.Fatalf("Processing ended because the reserved file does not exist %v\n", err)
+	}
+
+	fileDelete("img/*.png")
+
 }
 
+// 削除したいパス内のファイルを削除する
+func fileDelete(pattern string) {
+	files, err := filepath.Glob(pattern)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, file := range files {
+		if err := os.Remove(file); err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
 
 func main() {
-	driver := agouti.ChromeDriver()
+
+	driver := agouti.ChromeDriver(
+		agouti.ChromeOptions(
+			"args", []string{
+				"--headless", // browserを非表示で実行
+			}),
+	)
 
 	if err := driver.Start(); err != nil {
 		log.Fatalln(err)
